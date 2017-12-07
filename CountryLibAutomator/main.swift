909c0221cd039ab0626a5473344f12a2b4d0859a
Munir Wanis <fallen.angel.pp@gmail.com>
2017-12-07 18:58:47 -0200

    Initial Commit

---
+++ CountryLibAutomator/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  CountryLibAutomator
+//
+//  Created by Munir Wanis on 07/12/17.
+//  Copyright © 2017 Munir Wanis. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
